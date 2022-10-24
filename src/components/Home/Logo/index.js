524212f576e2cd055a5c453dcb3c81c4ee0ce748
Ben <ncor89b@gmail.com>
2022-10-24 20:39:13 +0200

    patch Implementation of the responsive layout

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,7 +2,6 @@ import { useRef, useEffect } from 'react'
 import './index.scss'
 import LogoLarge from '../../../assets/images/logo-portfolio-L.png'
 import gsap from 'gsap-trial'
-// import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
 
 const Logo = () => {
   const bgRef = useRef()
@@ -11,8 +10,6 @@ const Logo = () => {
 
   //: GSAP animation of the main logo
   useEffect(() => {
-    // gsap.registerPlugin(DrawSVGPlugin)
-
     gsap
       .timeline()
       .to(bgRef.current, {
